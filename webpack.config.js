a9637aa20e603a59a722a1a72c167ab9cf97ed42
ankkumar3 <akumar331@sapient.com>
2019-07-19 12:58:23 +0530

    Resolved typo from webconfig

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = (env) => {
         },
         plugins: [
             CSSExtract,
-            new webpack.Define({
+            new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
                 'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
